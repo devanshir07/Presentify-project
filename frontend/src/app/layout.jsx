126c6492e9cef33801fab15dfe866f88af983d26
divyanshir14 <divyanshirastogi63@gmail.com>
2025-05-06 13:52:55 +0530

    Comment out Navbar component in RootLayout

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         
-        <Navbar/>
+        {/* <Navbar/> */}
         <Toaster position="top-center"/>
         {children}
       </body>
